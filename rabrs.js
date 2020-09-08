ff526b1ca2ef09ae6efdd4d1654017d5593b03db
Romain Ambroise <romain.amb@gmail.com>
2020-09-08 20:35:34 +0200

    Add RD menu

--- rabrs.js
+++ rabrs.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    //$(".navigation").hide();
+    $(".navigation").hide();
+
+    $("#bouton_RD").click( function(){
+        if( $(".navigation").hasClass("drop-down") ) {
+            $(".navigation").hide();
+            $(".navigation").addClass("up");
+            $(".navigation").removeClass("drop-down");
+        } else {
+            $(".navigation").show();
+            $(".navigation").addClass("drop-down");
+            $(".navigation").removeClass("up");
+        }
+    });
 });
